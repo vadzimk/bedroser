acba479ef50326f0e22f8382d424e5a38064a5d6
Vadzim <vadzimk@hotmail.com>
2020-10-24 18:20:14 -0700

    first commit

---
+++ modules/PROJ_CONST.py
@@ -0,0 +1,14 @@
+DIR_PROJECT = './project_bed/'
+DIR_TABULATED_CSV = './project_bed/tabulated_csv/'
+DIR_PRODUCT_TABLES = './project_bed/dataframes/'
+DIR_TREATED_ROWS = './project_bed/treatedrows/'
+DIR_XPDF = './project_bed/html/'
+
+DOC_PRODUCT_TABLE = './project_bed/product_table.csv'
+DOC_TARGET = './project_bed/target.csv'
+DOC_UOM = './project_bed/uom.csv'
+
+TARGET_CONFIG = './TARGET_CONFIG.csv'
+
+OUTPUTXLS = 'output.xlsx'
+
